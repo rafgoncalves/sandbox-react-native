892469ddb6dc77b0f11dca2d242317853fdd44f3
rafgoncalves <rafael.augusto@gmail.com>
2019-04-30 09:07:51 -0300

    Added react-native-formik. New JSCore warnings

--- CustomFormDemo/custom_form_demo.js
+++ CustomFormDemo/custom_form_demo.js
@@ -1,6 +1,49 @@
-import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import React, {Component, PureComponent} from 'react';
+import {View, Text, Button, TextInput, Switch as RNSwitch} from 'react-native';
 import Styles from '../styles';
+import { Formik } from 'formik';
+import { withFormikControl } from 'react-native-formik';
+
+// TODO: https://codeburst.io/react-native-and-forms-redeux-part-1-9716f11b7ace
+
+const Switch  = withFormikControl(class extends React.PureComponent {
+    render() {
+      const { error, value, setFieldValue, label } = this.props;
+
+      return (
+        <React.Fragment>
+          <RNSwitch
+            value={value}
+            ios_backgroundColor={error ? "red" : "transparent"}
+            onValueChange={setFieldValue}
+          />
+          <Text>{label}</Text>
+        </React.Fragment>
+      );
+    }
+  }
+);
+
+const MyReactNativeForm = props => (
+  <Formik
+    initialValues={{ email: '' }}
+    onSubmit={values => console.log(values)}
+  >
+    {props => (
+      <View>
+        <TextInput
+          onChangeText={props.handleChange('email')}
+          onBlur={props.handleBlur('email')}
+          value={props.values.email}
+        />
+
+        <Switch label="Accept terms and conditions" name="termsAndConditionsAccepted" />
+
+        <Button onPress={props.handleSubmit} title="Submit" />
+      </View>
+    )}
+  </Formik>
+);
 
 export default class CustomForm extends Component {
   static navigationOptions = {
@@ -10,7 +53,9 @@ export default class CustomForm extends Component {
   render() {
     return (
       <View style={Styles.container}>
-        <Text style={Styles.welcome}>Oh no, you didn't code Custom Form yet!</Text>
+        <Text style={Styles.welcome}>Hey look, there's a form!</Text>
+        <MyReactNativeForm
+        />
       </View>
     );
   }
